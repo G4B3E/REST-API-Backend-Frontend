db1c8f37d5e195c51a5ffc5063080eb0ca7dc8bb
Petrik Diák <petrik@diak.hu>
2023-02-14 10:25:22 +0100

    validácio

--- src/newMerevlemez.dto.ts
+++ src/newMerevlemez.dto.ts
@@ -1,7 +1,21 @@
+import {
+  IsString,
+  Max,
+  MaxLength,
+  maxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
+
 export default class newMerevlemezDto {
+  @Min(1)
   tarhely: number;
 
+  @IsString()
+  @MinLength(1)
+  @MaxLength(10)
   marka: string;
 
+  @Min(1)
   sebesseg: number;
 }
