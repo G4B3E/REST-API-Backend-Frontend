efcac1b0e994a6b58439ec0b1f78927350764da3
Petrik Diák <petrik@diak.hu>
2023-02-14 10:35:30 +0100

    validácio 2.0

--- src/newMerevlemez.dto.ts
+++ src/newMerevlemez.dto.ts
@@ -8,14 +8,14 @@ import {
 } from 'class-validator';
 
 export default class newMerevlemezDto {
-  @Min(1)
+  @Min(1, { message: 'A szám nem lehetet kevesebb, mint 1!' })
   tarhely: number;
 
-  @IsString()
-  @MinLength(1)
-  @MaxLength(10)
+  @IsString({ message: 'A tartalomnak szövegnek kell lennie!' })
+  @MinLength(1, { message: 'A szöveg nem lehetet kevesebb, mint 1 karakter!' })
+  @MaxLength(10, { message: 'A szöveg nem lehetet nagyobb, mint 10 karakter!' })
   marka: string;
 
-  @Min(1)
+  @Min(1, { message: 'A szám nem lehetet kevesebb, mint 1!' })
   sebesseg: number;
 }
