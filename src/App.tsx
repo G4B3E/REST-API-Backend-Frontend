34fbd4bd23e09e009d5b875e6d84275db401a211
Petrik Diák <petrik@diak.hu>
2023-02-14 13:12:25 +0100

    frontend+

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    <table>
-      
-    </table>
+interface State {
+  merevlemezek: Merevlemez[];
+  tarhely: number;
+  marka: string;
+  sebesseg: number;
+}
 
-    
-    </div>
-  );
+interface Merevlemez {
+  tarhely: number;
+  marka: string;
+  sebesseg: number;
+}
+
+interface MerevlemezListResponse {
+  merevlemezek: Merevlemez[];
+}
+
+
+class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      tarhely:0,
+      marka: '',
+      sebesseg: 0,
+      merevlemezek: [],
+      
+    }
+  }
+
+  async loadMerevlemezek() {
+    let response = await fetch('http://localhost:3000/merevlemezek');
+    let data = await response.json() as MerevlemezListResponse;
+    this.setState({
+      merevlemezek: data.merevlemezek,
+    })
+  }
+  componentDidMount() {
+    this.loadMerevlemezek();
+  }
+  render() {
+    const { tarhely, marka, sebesseg } = this.state;
+
+    return <div>
+      <ul>
+        {
+          
+        }
+      </ul>
+    </div>;
+  }
 }
 
 export default App;
